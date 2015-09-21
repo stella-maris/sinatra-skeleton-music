34cfd203711da1f168fc301b817ae104046ce366
Mary Lee <mango.gal@gmail.com>
2015-09-21 21:57:48 +0000

    adds song details page link

--- app/actions.rb
+++ app/actions.rb
@@ -8,3 +8,21 @@ get '/music' do
   erb :'music/index'
 end
 
+get '/music/new' do
+  erb :'music/new'
+end
+
+post '/music' do
+  @music = Song.new(
+    song_title: params[:song_title],
+    author: params[:author],
+    url: params[:url]
+  )
+  @music.save
+  redirect '/music'
+end
+
+get '/music/:id' do
+  @music = Song.find params[:id]
+  erb :'music/show'
+end
